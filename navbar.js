f70d97560bc07435391a6bc5c86bc5dc0fe46583
-unset <-unset>
2019-03-22 15:48:26 +0100

    Zadanie2

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let navigation = "<h1>Nawigacja</h1><a href='kontakt.html'>Kontakt</a> <a href='index.html'>Index</a><br><br>";
+
+navbar = document.querySelector(".nav-bar");
+
+navbar.innerHTML = navigation;
